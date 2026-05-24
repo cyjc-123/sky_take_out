e40832c307da6266d668991f2fd9a1f33b785d50
wcy <123@wcy.cn>
2026-05-24 12:54:58 +0800

    缓存菜品业务功能开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -9,9 +9,11 @@ import com.sky.service.DishService;
 import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -20,22 +22,29 @@ import java.util.List;
 public class DishController {
    @Autowired
     private DishService dishService;
+   @Autowired
+   private RedisTemplate redisTemplate;
     @PostMapping
     public Result insert(@RequestBody DishDTO dishDTO){
          log.info("新增菜品：{}",dishDTO);
          dishService.insertWithFlavor(dishDTO);
+          String key="dish_"+dishDTO.getCategoryId();
+         cleanCache(key);
         return Result.success();
+
     }
     @GetMapping("/page")
     public Result selectPage(DishPageQueryDTO dishPageQueryDTO){
           log.info("分页查询：{}",dishPageQueryDTO);
           PageResult pageResult=dishService.selectPage(dishPageQueryDTO);
+
         return Result.success(pageResult);
     }
     @DeleteMapping
     public Result delete(@RequestParam List<Long> ids){
         log.info("删除菜品：{}",ids);
         dishService.deleteWithFlavor(ids);
+        cleanCache("dish_*");
         return Result.success();
     }
     @GetMapping("/{id}")
@@ -48,12 +57,14 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
              log.info("修改菜品：{}",dishDTO);
              dishService.updateWithFlavor(dishDTO);
+        cleanCache("dish_*");
              return Result.success();
     }
      @PostMapping("/status/{status}")
-    public Result status( @PathVariable Integer status,@RequestParam Long id){
+    public Result status( @PathVariable Integer status, Long id){
          log.info("状态：{},{}",status,id);
          dishService.ifStatus(status,id);
+         cleanCache("dish_*");
          return Result.success();
     }
     @GetMapping("/list")
@@ -61,5 +72,9 @@ public class DishController {
         log.info("根据分类查询菜品：{}",categoryId);
         List<Dish> list=dishService.listByCategoryId(categoryId);
         return Result.success(list);
+    }
+    private void cleanCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 }
